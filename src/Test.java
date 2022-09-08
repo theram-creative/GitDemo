e2d8b177fdb9176b3a6a6f3eb1efcc70a748f96c
RAM <RAM@DESKTOP-7AOKQ3J>
2022-09-08 17:25:16 +0530

    First commit..

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if(hungry =="yes")
+			System.out.println("eat something");
+		else
+			System.out.println("Do work");
+	}
+
+}
