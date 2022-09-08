f81a385de2f74d9b3aa69a2c6d3471f527fa5c80
RAM <RAM@DESKTOP-7AOKQ3J>
2022-09-08 18:55:12 +0530

    Added additional checks

--- src/Test2.java
+++ src/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 		
 		String hungry = "yes";
 		
-		if(hungry =="yes")
+		if(hungry =="yes" || hungry=="y" || hungry=="Y")
 			System.out.println("eat something");
 		else
 			System.out.println("Do work");
